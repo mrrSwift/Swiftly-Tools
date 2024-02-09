91b096eabf5feffef55103ecefe1959dacd87055
Swift <amirshayan1381@yahoo.com>
2024-02-10 00:24:43 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,52 +1,51 @@
-const {color} = require('./src/color');
-const {badWords} = require('./src/badWords');
+const { color } = require('./src/color');
+const { badWords } = require('./src/badWords');
 const { country, state, city, iranCities } = require('./src/countryAPI');
-const {expressValidation} = require('./src/express-validator');
-const {middleware} = require('./src/middleware');
-const {number} = require('./src/number');
+const { expressValidation } = require('./src/express-validator');
+const { middleware } = require('./src/middleware');
+const { number } = require('./src/number');
 const rs = require('./src/response');
-const {_sr} = require('./src/serverError');
-const {string} = require('./src/string');
-const {validate} = require('./src/validate');
-const {array} = require('./src/array');
-const {object} = require('./src/object');
-const {utils} = require('./src/utils');
-const {bcrypt} = require('./src/encrypt/bcrypt');
-const {crypto} = require('./src/encrypt/crypto');  
-const {iran} = require('./src/region');
-module.exports ={
+const { _sr } = require('./src/serverError');
+const { string } = require('./src/string');
+const { validate } = require('./src/validate');
+const { array } = require('./src/array');
+const { object } = require('./src/object');
+const { utils } = require('./src/utils');
+const { bcrypt } = require('./src/encrypt/bcrypt');
+const { crypto } = require('./src/encrypt/crypto');
+const { iran } = require('./src/region');
+module.exports = {
     color,
     badWords,
     bcrypt,
     crypto,
-    countries:{
-        country(){
-            return country()
-        },
-        state(){
-            return state()
-        },
-        city(){
-            return city()
-        },
-        iranCities(){
-            return iranCities()
-        }
-
-    },
     expressValidation,
     middleware,
     number,
     string,
     validate,
-    region:{
-        iran
-    },
     rs,
     _sr,
     array,
     object,
     utils,
+    region: {
+        iran
+    },
+    countries: {
+        country() {
+            return country()
+        },
+        state() {
+            return state()
+        },
+        city() {
+            return city()
+        },
+        iranCities() {
+            return iranCities()
+        }
 
+    },
 }
 
