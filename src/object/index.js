5ff27ec3ad49d616cdb0f1dce8cd9f7d3383c765
Swift <amirshayan1381@yahoo.com>
2024-02-22 22:47:23 +0330

    Update index.js

--- src/object/index.js
+++ src/object/index.js
@@ -1,27 +1,27 @@
 
-  module.exports.object = {
-/** 
-*@description Check obj and if obj is empty return true
-*@param obj 
-*@return Boolean value. if obj is empty return true
-*/
-    objectIsEmpty (obj = new Object(null))  {
-      //If it's not an array, return FALSE.
-      if (!Object.isFrozen(obj)) {
-        return false;
-      } if (Object.keys(obj).length === 0) {
-        //Return TRUE if the array is empty
-        return true;
-      }
-      //Otherwise, return FALSE.
+module.exports.object = {
+  /** 
+  *@description Check obj and if obj is empty return true
+  *@param {Object} obj 
+  *@return Boolean value. if obj is empty return true
+  */
+  objectIsEmpty(obj) {
+    //If it's not an array, return FALSE.
+    if (!Object.isFrozen(obj)) {
       return false;
-    },
-    /**
-   * 
-   * @param {Object} obj 
-   * @param {Object} defaults 
-   * @returns Merged object
-   */
+    } if (Object.keys(obj).length === 0) {
+      //Return TRUE if the array is empty
+      return true;
+    }
+    //Otherwise, return FALSE.
+    return false;
+  },
+  /**
+ * 
+ * @param {Object} obj 
+ * @param {Object} defaults 
+ * @returns Merged object
+ */
   merge(obj = {}, defaults) {
     for (const key in defaults) {
       if (typeof obj[key] === 'undefined') {
@@ -30,4 +30,4 @@
     }
     return obj;
   },
-  }
+}
